5d8d02c84143c0ddb45ce9b859336666c73f25f6
ayordev <117231730+ayordev@users.noreply.github.com>
2022-11-17 06:03:44 -0600

    Update drop.js

--- drop.js
+++ drop.js
@@ -1,3 +1,4 @@
+
 const actionButton = document.getElementById('action-button');
 const dropBox = document.getElementById('drop-box');
 const navButton = document.getElementById('nav-button');
@@ -10,6 +11,12 @@ const left = document.getElementById('left');
 const colorButtons = document.querySelectorAll('.color');
 
 let initialState = false;
+let color = localStorage.getItem('key');
+window.onload = () => {
+  if (!color) {
+    actionButton.innerText = 'purple dropdown';
+  }
+};
 
 function dropDown() {
   let color = localStorage.getItem('key');
@@ -22,6 +29,11 @@ function dropDown() {
     'position: absolute; top: 95%; left: 43%; transform: translate(-50%, -50%)',
   );
   actionButton.innerText = `${color} dropdown`;
+
+  if (!color) {
+    actionButton.innerText = 'purple dropdown';
+  }
+
   toggleDropBox();
   defaultDropBox();
 }
@@ -31,7 +43,13 @@ dropDown();
 function dropUp() {
   let color = localStorage.getItem('key');
   up.setAttribute('style', `background-color: ${color}; color: white`);
+  if (!color) {
+    up.style.color = 'white';
+    up.style.backgroundColor = 'purple';
+  }
   down.removeAttribute('style');
+  down.style.backgroundColor = 'white';
+  down.style.color = 'black';
   right.removeAttribute('style');
   left.removeAttribute('style');
   dropBox.setAttribute(
@@ -39,6 +57,10 @@ function dropUp() {
     'position: absolute; top: -13%; left: 43%; transform: translate(-50%, -50%)',
   );
   actionButton.innerText = `${color} dropup`;
+
+  if (!color) {
+    actionButton.innerText = 'purple dropup';
+  }
   toggleDropBox();
   defaultDropBox();
 }
@@ -46,14 +68,24 @@ function dropUp() {
 function dropRight() {
   let color = localStorage.getItem('key');
   right.setAttribute('style', `background-color: ${color}; color: white`);
+  if (!color) {
+    right.style.color = 'white';
+    right.style.backgroundColor = 'purple';
+  }
   up.removeAttribute('style');
   down.removeAttribute('style');
+  down.style.backgroundColor = 'white';
+  down.style.color = 'black';
   left.removeAttribute('style');
   dropBox.setAttribute(
     'style',
     'position: absolute; top: 46.5%; right: 28%; transform: translate(-50%, -50%)',
   );
   actionButton.innerText = `${color} dropright`;
+
+  if (!color) {
+    actionButton.innerText = 'purple dropright';
+  }
   toggleDropBox();
   defaultDropBox();
 }
@@ -61,14 +93,24 @@ function dropRight() {
 function dropLeft() {
   let color = localStorage.getItem('key');
   left.setAttribute('style', `background-color: ${color}; color: white`);
+  if (!color) {
+    left.style.color = 'white';
+    left.style.backgroundColor = 'purple';
+  }
   up.removeAttribute('style');
   right.removeAttribute('style');
   down.removeAttribute('style');
+  down.style.backgroundColor = 'white';
+  down.style.color = 'black';
   dropBox.setAttribute(
     'style',
     'position: absolute; top: 46.5%; left: 27.5%; transform: translate(-50%, -50%)',
   );
   actionButton.innerText = `${color} dropleft`;
+
+  if (!color) {
+    actionButton.innerText = 'purple dropleft';
+  }
   toggleDropBox();
   defaultDropBox();
 }
@@ -83,7 +125,7 @@ function toggleDropBox() {
   }
 }
 
-function displayDropBox() {
+function displayDropBox(e) {
   if (!initialState) {
     initialState = true;
     dropBox.style.visibility = 'visible';
@@ -91,6 +133,7 @@ function displayDropBox() {
     initialState = false;
     dropBox.style.visibility = 'hidden';
   }
+  console.log(e);
 }
 
 function defaultDropBox() {
@@ -112,9 +155,9 @@ function saveColor(accessKey) {
 
 function pickColor(e) {
   saveColor(e.target.accessKey);
-  if (down.style.backgroundColor && down.innerText) {
-    down.style.backgroundColor = e.target.accessKey;
-    actionButton.innerText = `${e.target.accessKey} dropdown`;
+  if (left.style.backgroundColor && left.innerText) {
+    left.style.backgroundColor = e.target.accessKey;
+    actionButton.innerText = `${e.target.accessKey} dropleft`;
   } else if (up.style.backgroundColor && up.innerText) {
     up.style.backgroundColor = e.target.accessKey;
     actionButton.innerText = `${e.target.accessKey} dropup`;
@@ -122,8 +165,8 @@ function pickColor(e) {
     right.style.backgroundColor = e.target.accessKey;
     actionButton.innerText = `${e.target.accessKey} dropright`;
   } else {
-    left.style.backgroundColor = e.target.accessKey;
-    actionButton.innerText = `${e.target.accessKey} dropleft`;
+    down.style.backgroundColor = e.target.accessKey;
+    actionButton.innerText = `${e.target.accessKey} dropdown`;
   }
 
   actionButton.style.backgroundColor = e.target.accessKey;
